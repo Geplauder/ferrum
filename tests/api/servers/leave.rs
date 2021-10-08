7f0f9db3b4a63feaeb8032aef1f020bbefb9ac55
Fabian Vowie <fabian@vowie.me>
2021-10-08 18:51:40 +0200

    Add tests for leave server websocket messages

--- tests/api/servers/leave.rs
+++ tests/api/servers/leave.rs
@@ -1,6 +1,9 @@
 use actix_http::{encoding::Decoder, Payload};
+use ferrum_websocket::messages::WebSocketMessage;
 
-use crate::helpers::TestApplication;
+use crate::helpers::{
+    get_next_websocket_message, send_websocket_message, TestApplication, TestUser,
+};
 
 impl TestApplication {
     pub async fn delete_leave_server(
@@ -145,4 +148,86 @@ async fn leave_returns_400_when_user_is_owner_of_the_server() {
 
     // Assert
     assert_eq!(400, response.status().as_u16());
+}
+
+#[ferrum_macros::test(strategy = "UserAndOtherServer")]
+async fn leave_sends_delete_server_to_leaving_user() {
+    // Arrange
+    app.put_join_server(
+        app.test_server().id.to_string(),
+        Some(app.test_user_token()),
+    )
+    .await;
+
+    let (_response, mut connection) = app.websocket().await;
+
+    send_websocket_message(
+        &mut connection,
+        WebSocketMessage::Identify {
+            bearer: app.test_user_token(),
+        },
+    )
+    .await;
+
+    get_next_websocket_message(&mut connection).await; // Accept the "Ready" message
+
+    // Act
+    app.delete_leave_server(
+        app.test_server().id.to_string(),
+        Some(app.test_user_token()),
+    )
+    .await;
+
+    // Assert
+    let message = get_next_websocket_message(&mut connection).await;
+
+    match message {
+        Some(WebSocketMessage::DeleteServer { server_id }) => {
+            assert_eq!(app.test_server().id, server_id);
+        }
+        Some(fallback) => assert!(false, "Received wrong message type: {:#?}", fallback),
+        None => assert!(false, "Received no message"),
+    }
+}
+
+#[ferrum_macros::test(strategy = "UserAndOwnServer")]
+async fn leave_sends_delete_user_to_users_on_server() {
+    // Arrange
+    let other_user = TestUser::generate();
+    other_user.store(&app.db_pool).await;
+    let other_user_token = app.jwt.encode(other_user.id, other_user.email);
+
+    app.put_join_server(
+        app.test_server().id.to_string(),
+        Some(other_user_token.clone()),
+    )
+    .await;
+
+    let (_response, mut connection) = app.websocket().await;
+
+    send_websocket_message(
+        &mut connection,
+        WebSocketMessage::Identify {
+            bearer: app.test_user_token(),
+        },
+    )
+    .await;
+
+    get_next_websocket_message(&mut connection).await; // Accept the "Ready" message
+
+    // Act
+    app.delete_leave_server(app.test_server().id.to_string(), Some(other_user_token))
+        .await;
+
+    // Assert
+    let message = get_next_websocket_message(&mut connection).await;
+
+    match message {
+        Some(WebSocketMessage::DeleteUser { user_id, server_id }) => {
+            assert_eq!(other_user.id, user_id);
+            assert_eq!(app.test_server().id, server_id);
+        }
+        Some(fallback) => assert!(false, "Received wrong message type: {:#?}", fallback),
+        None => assert!(false, "Received no message"),
+    }
 }
